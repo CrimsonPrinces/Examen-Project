5579a4a97572c028bcf5e29555fd8a4b2593faf5
CrimsonPrinces <Games4Life2305@outlook.com>
2024-09-11 08:42:33 +0200

    Medewerkers page with basic unstyled menu

---
+++ Medewerker.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voedselbank Maaskantje Medewerkers</title>
+</head>
+<body>
+
+<?php
+
+echo "Voedselbank Maaskantje Medewerkers";
+?>
+<br>
+<a href='home.php'> Home </a>
+<br>
+<a href='Voedselpakket.php'> Voedselpakket </a>
+<br>
+<a href='Voorraad.php'> Voorraad </a>
+<br>
+<a href='Leveranchier.php'> Leveranchiers </a>
+<br>
+<a href='Klanten.php'> Klanten </a>
+<br>
+<a href='index.php'> Uitloggen </a>
+
+</body>
+</html>
