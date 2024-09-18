9b0c12aea057e8763bab34a718cc2dc3f670367a
CrimsonPrinces <Games4Life2305@outlook.com>
2024-09-18 14:07:23 +0200

    Basic designs for table needs to be recplicated to other tables

--- Switches.php
+++ Switches.php
@@ -4,7 +4,7 @@ require_once("db_login.php");
 
 switch($_SESSION["usertype"]){
     case 1:
-    echo "<a href='Medewerker.php' class= 'mx-5'> Medewerkers </a>";
+    echo "<a href='Medewerker.php' class= 'mx-5 hover:bg-blue-200'> Medewerkers </a>";
     echo "<a href='Leverancier.php' class= 'mx-5'> Leveranciers </a>";
     echo "<a href='Klanten.php' class='mx-5'> Klanten </a>";
     echo "<a href='Voorraad.php' class='mx-5'> Voorraad </a>";
